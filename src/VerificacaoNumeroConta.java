08b3aec80a200cf798ac906c22e6e709f84642cf
baaguimaraes <guimaraes_barbara@sicredi.com.br>
2024-08-02 14:59:09 -0300

    first commit

---
+++ src/VerificacaoNumeroConta.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+
+public class VerificacaoNumeroConta {
+   
+        public static void main(String[] args) {
+             Scanner scanner = new Scanner(System.in);
+     
+             try {
+                 String numero = scanner.next();
+                 verificarNumeroConta(numero);
+                 System.out.println("Numero de conta valido.");
+             } catch (IllegalArgumentException e) {
+                 System.out.println("Erro: " + e.getMessage());
+             } finally {
+                 scanner.close();
+             }
+         }
+     
+         private static void verificarNumeroConta(String numeroConta) {
+             int contadorDeNumero = numeroConta.length();
+             int numeroDigitado = 0;
+             for (int i = 0; i < contadorDeNumero; i++) {
+                 numeroDigitado++;
+             }
+             if (numeroDigitado != 8) {
+                 throw new IllegalArgumentException("Numero de conta invalido. Digite exatamente 8 digitos.");
+             }
+     
+         }
+     
+     }
